cf7a4b15cddb92d37500ac7c9dd569a8d17da3f3
Leprekus <leprekus@yahoo.com>
2022-12-24 13:58:56 -0800

    create class to handle prompts

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class HandlePrompt {
+    public:
+    int nextPrompt() {
+        return 0;
+    };
+};
+int prompt() {
+    int choice = 0;
+    cout << "Choose an option by typing a number: \n"
+            "1) option one \n"
+            "2) option two \n"
+            "3) option three\n"
+        << endl;
+    cin >> choice;
+
+    return choice;
+}
 int main () {
     //text adventure project
+    HandlePrompt myPromptHandler;
+    cout << "fragment of story \n";
+    int choice = prompt();
+    if(choice) {
+        myPromptHandler.nextPrompt(choice);
+    } else {
+        cout << choice;
+        cout << "please make sure your input is a a number\n"
+                "between 1 & 3"
+            << endl; 
+        cout << "input";
+    }
+
     return 0;
 }
